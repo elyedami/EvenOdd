1d91cf0b904095b0698aa9231e8caf285fa445ee
elyedami <133836881+elyedami@users.noreply.github.com>
2023-06-04 20:32:19 -0400

    Lab_04_04_EvenOdd

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+        int number = 23; // Hard-coded input value
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
